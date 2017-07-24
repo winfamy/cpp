8f50f30d94814604e7051c71994a3f042f628517
Winfamy <infamy@unyx.io>
2017-07-23 23:28:28 -0500

    finished length.cpp

---
+++ 1/exercises/length.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	int centimeters, yards, feet, inches;
+	cout << "Enter centimeters: ";
+	cin >> centimeters;
+
+	inches = centimeters/2.54;
+	feet = inches/12;
+	yards = feet/3;
+
+	cout << yards << " yard(s) " << feet % 3 << " feet (foot) and " << inches % 12 << " inch(es)" << endl;
+
+	return 0;
+}
