7b17ade3ef023a9d754cb12b061da9df8ebdccbf
Winfamy <infamy@unyx.io>
2017-07-23 23:43:03 -0500

    finished art

---
+++ 1/exercises/art.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string name;
+
+	cout << " .       . " << endl;
+	cout << " |\\_---_/| " << endl;
+	cout << "/   o_o   \\" << endl;
+	cout << "|    U    |" << endl;
+	cout << "\\  ._I_.  /" << endl;
+ 	cout << " `-_____-' " << endl;
+
+	cout << "Give it a name: ";
+	cin >> name;
+
+	cout << "Wow! I will call it a '" << name << "'!" << endl;
+
+	return 0;
+}
