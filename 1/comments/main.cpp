f62c162cdd3afa03e12a9b574cdea20434d02fb8
Winfamy <infamy@unyx.io>
2017-07-23 22:22:00 -0500

    basic shit

---
+++ 1/comments/main.cpp
@@ -0,0 +1,19 @@
+/*
+multi-line comments
+*/
+
+#include <iostream>
+// single line comments
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	cout << "Hello world \n";
+
+	cout << "Now let's tab\t out" << endl;
+
+	// backward slash for escape sequences
+
+	return 0;
+}
---
+++ 1/variables/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string name;
+	int num1, num2;
+	int sum, product, mod;
+	double quotient;
+
+	cout << "Enter name: ";
+	cin >> name;
+
+	cout << "Enter an integer: ";
+	cin >> num1;
+
+	cout << "Enter an integer: ";
+	cin >> num2;
+
+	cout << "Hello, " << name << endl;
+
+	cout << num1 + num2 << endl;
+	cout << num1 * num2 << endl;
+	cout << num1 % num2 << endl;
+	cout << (double)num1 / num2 << endl;
+
+	return 0;
+}
